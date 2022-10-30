1146e8b707beaab107707a9e3c690621547ea7ae
Юлия Пшеничная <yulenykaa@gmail.com>
2022-10-30 12:42:48 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,16 +3,12 @@ const BASE_URL = 'https://restcountries.com/v3.1/name';
 const fetchCountries = name => {
   return fetch(
     `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(response => {
-      if (!response.ok) {
-        if (response.status === 404) {
-            return Promise.reject(new Error());
-        }
-         throw new Error(response.status);
-      }
-      return response.json();
-    })
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 };
 
 export { fetchCountries };
